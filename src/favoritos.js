484e3e9fb8b86729d2d9ae6368f693a37520d963
mattsleao <matheusleaobh@gmail.com>
2022-11-14 19:41:33 -0300

    feat: favoritos.js

---
+++ src/favoritos.js
@@ -0,0 +1,15 @@
+let eventos;
+
+function exibirDados() {
+    nomeEvento.innerHTML = eventos[0].nome;
+    imagemEvento.innerHTML = eventos[0].imagem;
+    dataHora.innerHTML = eventos[0].data;
+    localEvento.innerHTML = eventos[0].local;
+}
+
+fetch("eventos.json")
+    .then((response) => response.json())
+    .then((json) => eventos = json)
+    .then(() => exibirDados())
+
+

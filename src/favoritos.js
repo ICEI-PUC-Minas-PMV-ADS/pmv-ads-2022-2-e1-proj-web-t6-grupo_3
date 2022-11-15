3de91956b753cf8a0507003e48a56a1a30243f9f
mattsleao <matheusleaobh@gmail.com>
2022-11-14 22:33:24 -0300

    perf:favoritos.html favoritos.js styleFavorito.css

--- src/favoritos.js
+++ src/favoritos.js
@@ -1,24 +1,23 @@
-{
-    let eventos;
+let eventos;
 
-    
-function exibirDados(){
-    if(eventos[0].favorito){
-        nomeEvento.innerHTML = eventos[0].nome;
-        imagemEvento.innerHTML = eventos[0].imagem;
-        dataHora.innerHTML = eventos[0].data;
-        localEvento.innerHTML = eventos[0].local;
+function exibirDados() {
+  var textoHTML = '';
+  for (let i = 0; i < eventos.length; i++) {
+    if (eventos[i].favorito) {
+      textoHTML += `<div>`;
+      textoHTML += `<div id="imagemEvento">${eventos[i].imagem}</div>`;
+      textoHTML += `<p id="dataHora">${eventos[i].data}</p>`;
+      textoHTML += `<h2 id="nomeEvento">${eventos[i].nome}</h2>`;
+      textoHTML += `<p id="localEvento">${eventos[i].local}</p>`;
+      textoHTML += `</div>`;
     }
+  }
+  var console = document.getElementById("container");
+    console.innerHTML = textoHTML;
 }
-
 
 
 fetch("eventos.json")
-    .then((response) => response.json())
-    .then((json) => eventos = json)
-    .then(() => exibirDados())
-
-}
-
-
-
+  .then((response) => response.json())
+  .then((json) => (eventos = json))
+  .then(() => exibirDados());

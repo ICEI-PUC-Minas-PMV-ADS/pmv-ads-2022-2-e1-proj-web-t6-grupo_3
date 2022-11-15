1873d71e7cb93bcb31204459a58ace6a95b998a0
mattsleao <matheusleaobh@gmail.com>
2022-11-15 00:00:35 -0300

    feat: favoritos.js styleFavoritos.css

--- src/favoritos.js
+++ src/favoritos.js
@@ -4,11 +4,14 @@ function exibirDados() {
   var textoHTML = '';
   for (let i = 0; i < eventos.length; i++) {
     if (eventos[i].favorito) {
-      textoHTML += `<div>`;
+      textoHTML += `<div id="cards">`;
       textoHTML += `<div id="imagemEvento">${eventos[i].imagem}</div>`;
       textoHTML += `<p id="dataHora">${eventos[i].data}</p>`;
       textoHTML += `<h2 id="nomeEvento">${eventos[i].nome}</h2>`;
       textoHTML += `<p id="localEvento">${eventos[i].local}</p>`;
+      textoHTML += `<div id=button>`;
+      textoHTML += `<input type="button" id="btRemover" value="Remover">`;
+      textoHTML += `</div id=button>`;
       textoHTML += `</div>`;
     }
   }
@@ -21,3 +24,7 @@ fetch("eventos.json")
   .then((response) => response.json())
   .then((json) => (eventos = json))
   .then(() => exibirDados());
+
+
+
+
